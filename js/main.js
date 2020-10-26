6c8cc55fefaf1397c9ee21d588f7b9a9ebd28956
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2020-10-26 03:43:00 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
+$(document).ready(function () {
+
+});
+
+
 $(document).ready(function(){
+      //Start Mack button Scroll Top
+      $(window).scroll(function () {
+            if ($(this).scrollTop() >= 500) {
+                  if( $('.scroll-top').is(':hidden') ) {
+                  $('.scroll-top').fadeIn(400);
+                  }
+                  
+            } else {
+                  $('.scroll-top').fadeOut(400);
+            }
+
+            });
+            $('.scroll-top').click(function(e) {
+                  e.stopPropagation()
+            $("html,body").animate({
+                  scrollTop: 0
+            },1000);
+      });
    $('.owl-carousel').owlCarousel({
       rtl:true,
       loop: true,
